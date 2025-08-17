87c14b11459b44cb72c9db4787779f5932230fbc
kpmisthah <kpmisthah6@gmail.com>
2025-08-17 21:21:20 +0530

    feat(user):implement mapping to save data to db and return data form

---
+++ src/infrastructure/mappers/user.mapper.ts
@@ -0,0 +1,40 @@
+import { User,$Enums } from "@prisma/client";
+import { UserEntity } from "src/domain/entities/user.entity";
+import { Role } from "src/domain/enums/role.enum";
+export class UserMapper {
+    static toDomain(user:User):UserEntity{
+        return new UserEntity(
+            user.id,
+            user.name,
+            user.email,
+            user.password,            
+            user.role as Role,
+            user.isBlock,
+            user.isVerified,
+            user.profileImage ?? '',
+            user.bannerImage ?? '',
+            user.refreshToken ?? ''
+            
+
+        )
+    }
+
+    static toPrisma(user:UserEntity):Omit<User,'id'|'createdAt'|'updatedAt'>{
+        return {
+            email:user.email,
+            password:user.password,            name:user.name,
+            role:user.role as $Enums.Role,
+            isBlock:user.isBlock,
+            profileImage:user.profileImage,
+            bannerImage:user.bannerImage,
+            refreshToken:user.refreshToken,
+            isVerified:user.isVerified
+        }
+    }
+
+    static toDomainMany(users:User[]):UserEntity[]{
+        return users.map((user)=>{
+            return UserMapper.toDomain(user)
+        })
+    }
+}
