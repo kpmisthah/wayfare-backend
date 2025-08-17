7ccef5a0d78afb44814b14cf344a83a5a9e9f4b3
kpmisthah <kpmisthah6@gmail.com>
2025-08-17 21:13:56 +0530

    feat(user):implement create user and update user dto

---
+++ src/application/dtos/create-user.dto.ts
@@ -0,0 +1,23 @@
+
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Role } from 'src/domain/enums/role.enum';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @MinLength(6)
+  password: string | null;
+
+  @IsString()
+  refreshToken?: string 
+
+  role:Role
+
+  isBlock?:boolean
+}
