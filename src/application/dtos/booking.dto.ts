2dbadf7de03bc5c19d7e0766f15ab8c00a6e4480
kpmisthah <kpmisthah6@gmail.com>
2025-10-13 16:29:16 +0530

    feat(booking): add booking-related DTOs for create, fetch, and status operations

---
+++ src/application/dtos/booking.dto.ts
@@ -0,0 +1,19 @@
+import { Type } from "class-transformer";
+import { IsDateString, IsNumber, IsString } from "class-validator";
+
+
+export class BookingDto {
+
+    @IsString()
+    packageId:string
+    @IsDateString()
+    travelDate:string
+    @IsNumber()
+    @Type(()=>Number)
+    peopleCount:number
+    @Type(()=>Number)
+    totalAmount:number
+    @IsString()
+    paymentType?:string
+
+}
