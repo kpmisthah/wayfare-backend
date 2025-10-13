a574e057f49b4aa2d5e0212c514e372b323347de
kpmisthah <kpmisthah6@gmail.com>
2025-10-13 17:22:40 +0530

    feat(transaction):implement wallet repository

---
+++ src/infrastructure/database/prisma/repositories/wallet/wallet.transaction.repository.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { BaseRepository } from "../base.repository";
+import { WalletTransactionEntity } from "src/domain/entities/wallet-transaction.entity";
+import { IWalletTransactionRepository } from "src/domain/repositories/wallet/wallet-transaction.repository.interface";
+import { PrismaService } from "../../prisma.service";
+import { WalletTransactionMapper } from "src/infrastructure/mappers/wallet-transaction.mapper";
+
+@Injectable()
+export class WalletTransactionRepository extends BaseRepository<WalletTransactionEntity> implements IWalletTransactionRepository {
+    constructor(private readonly _prisma:PrismaService){
+        super(_prisma.walletTransaction,WalletTransactionMapper)
+    }
+}
