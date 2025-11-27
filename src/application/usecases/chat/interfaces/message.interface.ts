fe5508f207ed2d136e27c165562ceb86cb816970
kpmisthah <kpmisthah6@gmail.com>
2025-11-27 23:58:49 +0530

    feat(group-chat):implement interface for group message

--- src/application/usecases/chat/interfaces/message.interface.ts
+++ src/application/usecases/chat/interfaces/message.interface.ts
@@ -1,6 +1,12 @@
+import { GroupChatDto } from "src/application/dtos/group-chat.dto"
 import { MessageDto } from "src/application/dtos/message.dto"
 
 export interface IChatUsecase {
     saveMessages(conversationId:string,senderId:string,content:string):Promise<MessageDto|null>
     getMessages(conversationId:string)
+    createGroup(creatorId: string, dto: GroupChatDto)
+    saveGroupMessage(groupId: string,senderId: string,content: string): Promise<MessageDto>
+    getUserGroups(userId: string)
+    isGroupId(id: string): Promise<boolean>
+    getGroupMessages(groupId: string): Promise<MessageDto[]>
 }
