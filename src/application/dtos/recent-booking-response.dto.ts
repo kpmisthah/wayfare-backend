cbf2589cded660967c0f93a391f74bd928acc165
kpmisthah <kpmisthah6@gmail.com>
2025-11-30 17:58:37 +0530

    feat(admin-dashboard):implemnt response dto for recent booking

---
+++ src/application/dtos/recent-booking-response.dto.ts
@@ -0,0 +1,9 @@
+export class RecentBookingResponse {
+  id: string;
+  customerName: string;
+  agencyName: string;
+  destination: string;
+  amount: number;
+  status: string;
+  createdAt: Date;
+}
