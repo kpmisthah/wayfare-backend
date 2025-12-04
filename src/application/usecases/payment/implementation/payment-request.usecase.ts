34a1dad911389acf9ba65b7e033ad115d6ce61ef
kpmisthah <kpmisthah6@gmail.com>
2025-12-04 17:09:20 +0530

    feat(admin):Implement payout req usecase

--- src/application/usecases/payment/implementation/payment-request.usecase.ts
+++ src/application/usecases/payment/implementation/payment-request.usecase.ts
@@ -5,6 +5,10 @@ import { IPayoutRequestRepository } from 'src/domain/interfaces/payout-request.i
 import { PayoutRequestEntity } from 'src/domain/entities/payout-request.entity';
 import { PayoutRequestMapper } from '../../mapper/payout-request.mapper';
 import { ICreatePayoutRequestUsecase } from '../interfaces/create-payout.usecase.interface';
+import { PayoutDetailsDTO } from 'src/application/dtos/payout-details.dto';
+import { PayoutStatus } from 'src/domain/enums/payout-status.enum';
+import { StatusCode } from 'src/domain/enums/status-code.enum';
+import { ResponseDto } from 'src/application/dtos/reponse.dto';
 
 @Injectable()
 export class CreatePayoutRequestUsecase implements ICreatePayoutRequestUsecase {
@@ -24,5 +28,55 @@ export class CreatePayoutRequestUsecase implements ICreatePayoutRequestUsecase {
     console.log(result, 'resulttt');
     if (!result) return null;
     return PayoutRequestMapper.toDto(result);
+  }
+  async payoutDetails(
+    page: number,
+    limit: number,
+    status?: PayoutStatus,
+    search?: string,
+  ): Promise<{ data: PayoutDetailsDTO[]; total: number }> {
+    const skip = (page - 1) * limit;
+    const take = limit;
+    return await this._payoutRepo.payoutDetails({ skip, take, status, search });
+  }
+
+  async approvePayout(
+    id: string,
+    status: PayoutStatus,
+  ): Promise<ResponseDto | null> {
+    let payoutRequest = await this._payoutRepo.findById(id);
+    console.log(payoutRequest, 'payoutreq');
+    if (!payoutRequest) return null;
+    let updatePayoutRequest = payoutRequest.update({ status });
+    console.log(updatePayoutRequest, 'upated');
+
+    let res = await this._payoutRepo.update(
+      payoutRequest.id,
+      updatePayoutRequest,
+    );
+    console.log(res, '================>resutlltt===================');
+
+    return {
+      code: StatusCode.SUCCESS,
+      message: `Payout approved`,
+    };
+  }
+
+  async rejectPayout(id: string, reason: string):Promise<ResponseDto|null> {
+    let payout = await this._payoutRepo.findById(id);
+    if (!payout) return null;
+
+    let udpatePayout = payout.update({
+      status: PayoutStatus.REJECTED,
+      rejectionReason: reason,
+    });
+
+    let res = await this._payoutRepo.update(id, udpatePayout);
+    console.log(res,'reason for cancelllattionnnn');
+    
+    return {
+      code: StatusCode.SUCCESS,
+      message: 'Payout rejected',
+    };
   }
 }
