69ae522dde18f598cec8c6746a7c4f3017deb5df
kpmisthah <kpmisthah6@gmail.com>
2025-12-04 17:07:09 +0530

    fix(admin):fix admin usecase chnges for pagination in interface

--- src/application/usecases/admin/interfaces/admin.usecase.interface.ts
+++ src/application/usecases/admin/interfaces/admin.usecase.interface.ts
@@ -2,10 +2,16 @@ import { preference } from '@prisma/client';
 import { AgencyManagementDto } from 'src/application/dtos/agency-management.dto';
 import { PreferenceDto } from 'src/application/dtos/preferences.dto';
 import { SafeUser } from 'src/application/dtos/safe-user.dto';
+import { AgencyStatus } from 'src/domain/enums/agency-status.enum';
 
 export interface IAdminService {
   createPreference(preferenceDto: PreferenceDto): Promise<preference | null>;
   getAllPreferences(): Promise<preference[] | null>;
-  getAllAgencies(): Promise<AgencyManagementDto[] | null>;
+  getAllAgencies(dto: {
+      page: number;
+      limit: number;
+      search?: string;
+      status?: AgencyStatus;
+    }): Promise<{data:AgencyManagementDto[];total:number} | null> 
   findAdmin(): Promise<SafeUser | null>;
 }
