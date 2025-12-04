e936bb35485753800617bf9b6bd7ad8dedfd9aad
kpmisthah <kpmisthah6@gmail.com>
2025-12-04 17:11:02 +0530

    fix(agency):Implement interface for payout req

--- src/domain/interfaces/payout-request.interface.ts
+++ src/domain/interfaces/payout-request.interface.ts
@@ -1,7 +1,15 @@
+import { PayoutDetailsDTO } from 'src/application/dtos/payout-details.dto';
 import { PayoutRequestEntity } from '../entities/payout-request.entity';
 import { IBaseRepository } from '../repositories/base.repository';
+import { PayoutStatus } from '../enums/payout-status.enum';
 
 export interface IPayoutRequestRepository
   extends IBaseRepository<PayoutRequestEntity> {
   findPendingByAgencyId(agencyId: string): Promise<PayoutRequestEntity[]>;
+  payoutDetails(options: {
+    skip: number;
+    take: number;
+    status?: PayoutStatus;
+    search?: string;
+  }): Promise<{ data: PayoutDetailsDTO[], total: number }>
 }
