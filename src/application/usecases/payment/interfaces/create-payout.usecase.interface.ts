dfb418e09fab4ca57941379fa7a9268a63ec1424
kpmisthah <kpmisthah6@gmail.com>
2025-12-04 17:09:46 +0530

    feat(admin):Implement interface for payout-req

--- src/application/usecases/payment/interfaces/create-payout.usecase.interface.ts
+++ src/application/usecases/payment/interfaces/create-payout.usecase.interface.ts
@@ -1,5 +1,16 @@
+import { PayoutDetailsDTO } from 'src/application/dtos/payout-details.dto';
 import { PayoutRequestDto } from 'src/application/dtos/payout-request.dto';
+import { ResponseDto } from 'src/application/dtos/reponse.dto';
+import { PayoutStatus } from 'src/domain/enums/payout-status.enum';
 
 export interface ICreatePayoutRequestUsecase {
   execute(dto: PayoutRequestDto): Promise<PayoutRequestDto | null>;
+  payoutDetails(
+    page: number,
+    limit: number,
+    status?: PayoutStatus,
+    search?: string,
+  ): Promise<{ data: PayoutDetailsDTO[]; total: number }> 
+  approvePayout(id:string,status:PayoutStatus):Promise<ResponseDto|null>
+  rejectPayout(id: string, reason: string):Promise<ResponseDto|null>
 }
