3f9c352e60408bd1440d5fb53a87aaf65dce40a8
kpmisthah <kpmisthah6@gmail.com>
2025-12-07 14:10:33 +0530

    feat(notification):Implement presentation layer for notification

---
+++ src/presentation/notification/notification.module.ts
@@ -0,0 +1,19 @@
+import { forwardRef, Module } from "@nestjs/common";
+import { NotificationController } from "./notification.controller";
+import { NotificationUsecase } from "src/application/usecases/notification/implementation/notification.usecase";
+import { ChatModule } from "../chat/chat.module";
+
+@Module({
+    imports:[forwardRef(()=>ChatModule)],
+    controllers:[NotificationController],
+    providers:[
+        {
+            provide:"INotificationUsecase",
+            useClass:NotificationUsecase
+            
+        }
+    ],
+    exports:['INotificationUsecase']
+})
+
+export class NotificationModule{}
