dc46459b8cc73d2787c28ede8e36814b10dac4d5
kpmisthah <kpmisthah6@gmail.com>
2025-12-07 14:11:37 +0530

    feat(notification):Implement interface for repository notification

---
+++ src/domain/repositories/user/notification.repository.interface.ts
@@ -0,0 +1,8 @@
+import { NotificationEntity } from "src/domain/entities/notification.entity";
+import { IBaseRepository } from "../base.repository";
+
+export interface INotificationRepository extends IBaseRepository<NotificationEntity>{
+  findByUser(userId: string, limit?: number, offset?: number): Promise<NotificationEntity[]>;
+  markAsRead(notificationId: string): Promise<NotificationEntity | null>;
+  countUnread(userId: string): Promise<number>;
+}
