7dbc3e34f123ca81300452eca11b5edef4b110c1
kpmisthah <kpmisthah6@gmail.com>
2025-12-07 14:05:50 +0530

    feat(notification):Implement dto for notification

---
+++ src/application/dtos/create-notification.dto.ts
@@ -0,0 +1,12 @@
+import { IsString } from "class-validator";
+import { NotificationStatus } from "src/domain/enums/notification-status.enum";
+
+export class CreateNotificationDto {
+  @IsString()  
+  title: string;
+
+  @IsString()
+  message: string;
+
+  type: NotificationStatus
+}
