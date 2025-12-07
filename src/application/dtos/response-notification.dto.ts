92a7ab60a34dbf7d1f0cab6a9daef0c1733f66c1
kpmisthah <kpmisthah6@gmail.com>
2025-12-07 14:09:39 +0530

    feat(notification):Implement dto for response notificatoin

---
+++ src/application/dtos/response-notification.dto.ts
@@ -0,0 +1,19 @@
+import { IsString } from "class-validator";
+import { NotificationStatus } from "src/domain/enums/notification-status.enum";
+
+export class ResponseNotificationDto{
+  @IsString()  
+  id: string;
+
+  @IsString()
+  title: string;
+  
+  @IsString()
+  message: string;
+
+  unread: boolean;
+
+  date: string;
+
+  type:NotificationStatus
+}
