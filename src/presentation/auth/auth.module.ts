171bec56b0cf4fc8dd38d7b85dde02343437ad83
kpmisthah <kpmisthah6@gmail.com>
2025-12-16 13:35:07 +0530

    fix:(lint):fix some lint issues in auth module

--- src/presentation/auth/auth.module.ts
+++ src/presentation/auth/auth.module.ts
@@ -8,7 +8,6 @@ import { UsersModule } from '../users/users.module';
 import { AccessTokenGuard } from '../../infrastructure/common/guard/accessToken.guard';
 import { RefreshTokenGuard } from '../../infrastructure/common/guard/refreshToken.guard';
 import { OtpModule } from '../otp/otp.module';
-// import { GoogleStrategy } from '../../application/usecases/auth/strategies/google.strategy';
 import { UserService } from '../../application/usecases/users/implementation/users.usecase';
 import { JwtTokenFactory } from '../../application/usecases/auth/implementation/jwt-token.factory';
 import { GoogleLoginUseCase } from '../../application/usecases/auth/implementation/google-login.usecase';
