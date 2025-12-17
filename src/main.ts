42cccb1a8ddcbd912bd3b58092b1e8f46cf9ffb5
kpmisthah <kpmisthah6@gmail.com>
2025-12-17 21:44:14 +0530

    fix:(main):cors issue solved finally

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
     app.use(cookieParser());
 
     app.enableCors({
-      origin: ['http://localhost:3000', 'https://wayfare-frontend.vercel.app/'],
+      origin: ['https://wayfare.misthah.site'],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     });
